5283625027b5000feaf84abbe4310fab95c09ca9
BAMaria99 <72960740+BAMaria99@users.noreply.github.com>
2021-01-06 18:18:36 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const news = require('news');
+const app = news();
+const newsRouter = require('./routers/news');
+
+app.use(express.static('public'));
+app.use(express.static('data/uploads'));
+app.use('/api', newsRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}.`);
+});
